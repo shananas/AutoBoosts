8c94d7d7330b0e85a86c1ec19591b0b8df4e3324
aliosgaming <84293623+aliosgaming@users.noreply.github.com>
2024-07-17 22:45:46 -0700

    Update F266B00B autoboosts.lua
    Updated for v.1.0.0.9 Steam and Epic Global

--- F266B00B autoboosts.lua
+++ F266B00B autoboosts.lua
@@ -1,34 +1,67 @@
+--Updated for v1.0.0.9 Steam and Epic Global
+
+local canExecute = false
+
 function _OnFrame()
-    World = ReadByte(Now + 0x00)
-    Room = ReadByte(Now + 0x01)
-    Place = ReadShort(Now + 0x00)
-    Door = ReadShort(Now + 0x02)
-    Map = ReadShort(Now + 0x04)
-    Btl = ReadShort(Now + 0x06)
-    Evt = ReadShort(Now + 0x08)
-    Cheats()
+	if canExecute == true then
+		World = ReadByte(Now + 0x00)
+		Room = ReadByte(Now + 0x01)
+		Place = ReadShort(Now + 0x00)
+		Door = ReadShort(Now + 0x02)
+		Map = ReadShort(Now + 0x04)
+		Btl = ReadShort(Now + 0x06)
+		Evt = ReadShort(Now + 0x08)
+		Cheats()
+	end
 end
 
 function _OnInit()
-    if GAME_ID == 0xF266B00B or GAME_ID == 0xFAF99301 and ENGINE_TYPE == "ENGINE" then--PCSX2
-        Platform = 'PS2'
+	if (GAME_ID == 0xF266B00B or GAME_ID == 0xFAF99301) and ENGINE_TYPE == "ENGINE" then --PCSX2
+		canExecute = true
+		Platform = 'PS2'
         Now = 0x032BAE0 --Current Location
         Save = 0x032BB30 --Save File
         Obj0 = 0x1C94100 --00objentry.bin
-	Cntrl = 0x1D48DB8
+		Cntrl = 0x1D48DB8
         Sys3 = 0x1CCB300 --03system.bin
         Btl0 = 0x1CE5D80 --00battle.bin
         Slot1 = 0x1C6C750 --Unit Slot 1
-    elseif GAME_ID == 0x431219CC and ENGINE_TYPE == 'BACKEND' then--PC
-        Platform = 'PC'
-        Now = 0x0714DB8 - 0x56454E
-        Save = 0x09A7070 - 0x56450E
-        Obj0 = 0x2A22B90 - 0x56450E
-	Cntrl = 0x2A148A8 - 0x56450E
-        Sys3 = 0x2A59DB0 - 0x56450E
-        Btl0 = 0x2A74840 - 0x56450E
-        Slot1 = 0x2A20C58 - 0x56450E
-    end
+		print('Emulator')
+	elseif GAME_ID == 0x431219CC and ENGINE_TYPE == 'BACKEND' then --PC
+		canExecute = true
+		if ReadByte(0x566A8E) == 0xFF then --EGS Global
+			Platform = 'PC-Epic'
+			Now = 0x0716DF8
+			Save = 0x09A92F0
+			Obj0Pointer = 0x2A24A70
+			Sys3Pointer = 0x2AE5890
+			Btl0Pointer = 0x2AE5898
+			Cntrl = 0x2A16C28
+			Obj0 = ReadLong(Obj0Pointer)
+			Sys3 = ReadLong(Sys3Pointer)
+			Btl0 = ReadLong(Btl0Pointer)
+			Slot1 = 0x2A22FD8
+			ConsolePrint('Epic Games Global')
+		elseif ReadByte(0x56668E) == 0xFF then --Steam Global
+			Platform = 'PC-Steam'
+			Now = 0x0717008
+			Save = 0x09A9830
+			Obj0Pointer = 0x2A24FB0
+			Sys3Pointer = 0x2AE5DD0
+			Btl0Pointer = 0x2AE5DD8
+			Cntrl = 0x2A17168
+			Obj0 = ReadLong(Obj0Pointer)
+			Sys3 = ReadLong(Sys3Pointer)
+			Btl0 = ReadLong(Btl0Pointer)
+			Slot1 = 0x2A23518
+			ConsolePrint('Steam Global')
+		else
+			canExecute = false
+			ConsolePrint('KH2 not detected, not running script')
+		end
+	else
+		ConsolePrint('KH2 not detected, not running script')
+	end
 end
 
 function Events(M,B,E) --Check for Map, Btl, and Evt
